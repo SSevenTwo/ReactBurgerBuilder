3940f2b1975b1f821665d068b6f5344b2680a315
Ian Nguyen <iannguyen172@gmail.com>
2020-03-10 20:52:49 +1100

    Added total price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,9 @@ const controls = [
 const buildControls = props => {
   return (
     <div className={classes.BuildControls}>
+      <p>
+        Total Price: <strong>{props.price.toFixed(2)}</strong>
+      </p>
       {controls.map(ingredient => {
         return (
           <BuildControl
