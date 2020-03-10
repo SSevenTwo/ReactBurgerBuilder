25408131d27fbb5ed742b77ced756287e81ad135
Ian Nguyen <iannguyen172@gmail.com>
2020-03-10 18:33:56 +1100

    Added Burger and Burger Ingredient Component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/ReactAux";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger Picture</div>
+        <div>Ingredient Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
